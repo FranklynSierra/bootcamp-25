95bfd0391df6bc14cf3a4997a67d2b84d961accf
fran <franklynssierracontreras@gmail.com>
2022-06-25 14:44:01 -0400

    proyecto 25 de openBootcamp

---
+++ index.js
@@ -0,0 +1,34 @@
+const boton=document.getElementById('boton')
+
+boton.addEventListener('click',()=>{
+    alert('click en el botón');
+
+})
+
+
+$('button').click(()=>{
+   console.log('Hola, estoy utilizando jQuery')
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
